7c890d5c3b5b420f1db37b02d1ac63c7d49c608e
Zoren <zorenleolumosbog@gmail.com>
2023-02-09 12:22:51 +0800

    commit

---
+++ resources/views/partials/guest/header.blade.php
@@ -0,0 +1,14 @@
+<header class="main-header">
+    <a href="{{ route('guest.home') }}" class="logo">
+        <span class="logo-mini">Events Booking System</span>
+        <span class="logo-lg">Events Booking System</span>
+    </a>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+
+            <ul class="nav navbar-nav navbar-right" style="margin-right: 50px;">
+                <li><a href="{{ url('/login') }}">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
